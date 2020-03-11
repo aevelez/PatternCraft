fffb98f72b0ec7a0b9064b4787dde68b8eeffaad
Alex Edwin Velez Velez <aleno23@outlook.com>
2020-03-11 11:49:28 -0500

    Add project files.

---
+++ PatternCraft.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PatternCraft.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PatternCraft.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90bb26a9-96c2-40c2-b5a3-e0145bea02cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PatternCraft/StatePattern.cs
@@ -0,0 +1,56 @@
+﻿namespace PatternCraft
+{
+    
+
+    public interface IUnit
+    {
+        IUnitState State { get; set; }
+        bool CanMove { get; }
+        int Damage { get; }
+    }
+
+    public interface IUnitState
+    {
+        bool CanMove { get; set; }
+        int Damage { get; set; }
+    }
+
+    public class SiegeState : IUnitState
+    {
+        public SiegeState()
+        {
+            CanMove = false;
+            Damage = 20;
+        }
+
+        public bool CanMove { get; set; }
+        public int Damage { get; set; }
+    }
+
+    public class TankState : IUnitState
+    {
+        public TankState()
+        {
+            CanMove = true;
+            Damage = 5;
+        }
+
+        public bool CanMove { get; set; }
+        public int Damage { get; set; }
+    }
+
+    public class Tank : IUnit
+    {
+        public Tank()
+        {
+            State = new TankState();
+        }
+
+        public IUnitState State { get; set; }
+
+        public bool CanMove { get { return State.CanMove; } }
+        public int Damage { get { return State.Damage; } }
+    }
+
+   
+}
