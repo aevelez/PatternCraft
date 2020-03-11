d0bb22ec5e21c852db4c2fbdae99163ec3cc6e5d
Marco Giraldo <magiraldo1@eafit.edu.co>
2020-03-11 13:41:14 -0500

    Decorator Pattern

---
+++ PatternCraft.Test/KataTestDecorator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PatternCraft.Test
+{
+    [TestClass]
+    public class KataTestDecorator
+    {
+        [TestMethod]
+        public void _0_SingleUpgrade()
+        {
+            IMarine marine = new Marine(10, 1);
+
+            Assert.AreEqual(11, new MarineWeaponUpgrade(marine).Damage);
+            Assert.AreEqual(11, new MarineWeaponUpgrade(marine).Damage);
+        }
+
+        [TestMethod]
+        public void _1_DoubleUpgrade()
+        {
+            IMarine marine = new Marine(15, 1);
+            marine = new MarineWeaponUpgrade(marine);
+            marine = new MarineWeaponUpgrade(marine);
+
+            Assert.AreEqual(17, marine.Damage);
+        }
+
+        [TestMethod]
+        public void _2_TripleUpgrade()
+        {
+            IMarine marine = new Marine(20, 1);
+            marine = new MarineWeaponUpgrade(marine);
+            marine = new MarineWeaponUpgrade(marine);
+            marine = new MarineWeaponUpgrade(marine);
+
+            Assert.AreEqual(23, marine.Damage);
+        }
+    }
+}
---
+++ PatternCraft/DecoratorPattern.cs
@@ -0,0 +1,50 @@
+﻿namespace PatternCraft
+{
+    public interface IMarine
+    {
+        int Damage { get; set; }
+        int Armor { get; set; }
+    }
+
+    public class Marine : IMarine
+    {
+        public Marine(int damage, int armor)
+        {
+            Damage = damage;
+            Armor = armor;
+        }
+
+        public int Damage { get; set; }
+        public int Armor { get; set; }
+    }
+
+    public class MarineWeaponUpgrade : IMarine
+    {
+        private IMarine marines;
+
+        public MarineWeaponUpgrade(IMarine marine)
+        {
+            marines = marine;
+            marines.Damage ++;
+        }
+
+        public int Damage { get { return marines.Damage; } set { marines.Damage = value; } }
+
+        public int Armor { get { return marines.Armor; } set { marines.Armor = value; } }
+    }
+
+    public class MarineArmorUpgrade : IMarine
+    {
+        private IMarine marines;
+
+        public MarineArmorUpgrade(IMarine marine)
+        {
+            marines = marine;
+            marines.Armor++;
+        }
+
+        public int Damage { get { return marines.Damage; } set { marines.Damage = value; } }
+
+        public int Armor { get { return marines.Armor; } set { marines.Armor = value; } }
+    }
+}
