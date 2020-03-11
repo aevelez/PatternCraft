037297daaaff1d1bc6404ea2df11543370e154d3
Alex Edwin Velez Velez <aleno23@outlook.com>
2020-03-11 12:18:50 -0500

    Add Visitor Pattern

---
+++ PatternCraft.Test/KataTestVisitorPattern.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatternCraft.Visitor;
+
+namespace PatternCraft.Test
+{
+    [TestClass]
+    public class KataTestVisitorPattern
+    {
+        [TestMethod]
+        public void _0_VisitLight()
+        {
+            IVisitor bullet = new TankBullet();
+            ILightUnit light = new Marine();
+
+            light.Accept(bullet);
+
+            Assert.AreEqual(100 - 21, light.Health);
+        }
+
+        [TestMethod]
+        public void _1_VisitArmored()
+        {
+            IVisitor bullet = new TankBullet();
+            IArmoredUnit armored = new Marauder();
+
+            armored.Accept(bullet);
+
+            Assert.AreEqual(125 - 32, armored.Health);
+        }
+    }
+}
---
+++ PatternCraft/VisitorPattern.cs
@@ -0,0 +1,58 @@
+﻿
+namespace PatternCraft.Visitor
+{
+  
+
+    public interface IVisitor
+    {
+        void VisitLight(ILightUnit unit);
+        void VisitArmored(IArmoredUnit unit);
+    }
+
+    public interface ILightUnit
+    {
+        int Health { get; set; }
+
+        void Accept(IVisitor visitor);
+    }
+
+    public interface IArmoredUnit
+    {
+        int Health { get; set; }
+
+        void Accept(IVisitor visitor);
+    }
+
+    public class Marine : ILightUnit
+    {
+        public int Health { get; set; } = 100;
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitLight(this);
+        }
+    }
+
+    public class Marauder : IArmoredUnit
+    {
+        public int Health { get; set; } = 125;
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitArmored(this);
+        }
+    }
+
+    public class TankBullet : IVisitor
+    {
+        public void VisitLight(ILightUnit unit)
+        {
+            unit.Health -= 21;
+        }
+
+        public void VisitArmored(IArmoredUnit unit)
+        {
+            unit.Health -= 32;
+        }
+    }
+}
